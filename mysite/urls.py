44fb49be6a268404e3fdb82a209cf23c620ba622
badhashsolution <badhashsolution@gmail.com>
2013-12-22 13:17:29 -0800

    working - tutorial 3: 'see the text "Hello, world. You're at the poll index."'

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
